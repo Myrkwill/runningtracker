c96d6dffcfe3b48a55d9aaa22f5243ba0173a82a
Mark Nagibin <myrkwill@icloud.com>
2023-03-14 12:25:06 +0500

    Add converters and database

---
+++ app/src/main/java/ru/myrkwill/runningapp/db/Converters.kt
@@ -0,0 +1,21 @@
+package ru.myrkwill.runningapp.db
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    @TypeConverter
+    fun toBitmap(bytes: ByteArray): Bitmap {
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+    }
+
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val outputSteam = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputSteam)
+        return outputSteam.toByteArray()
+    }
+}
