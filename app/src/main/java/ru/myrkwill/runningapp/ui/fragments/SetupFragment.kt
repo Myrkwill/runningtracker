befcbb32042f183288cac8e9eab7eea568b7dc0f
Mark Nagibin <myrkwill@icloud.com>
2023-03-14 18:12:31 +0500

    Add setup fragment

---
+++ app/src/main/java/ru/myrkwill/runningapp/ui/fragments/SetupFragment.kt
@@ -0,0 +1,27 @@
+package ru.myrkwill.runningapp.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ru.myrkwill.runningapp.databinding.FragmentSetupBinding
+
+@AndroidEntryPoint
+class SetupFragment : Fragment() {
+
+    private lateinit var binding: FragmentSetupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSetupBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
